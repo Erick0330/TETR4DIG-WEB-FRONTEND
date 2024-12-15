de6f0662901a24160e7a896b26eeceda81106180
Glichoh <165095363+Glichoh@users.noreply.github.com>
2024-12-15 12:23:19 -0500

    borrando el header y to lo demas q no hace falta

--- src/components/QuiestionsComponent.vue
+++ src/components/QuiestionsComponent.vue
@@ -2,9 +2,9 @@
 </script>
 
 <template>
-   <!--Tabla de Consultoria-->
+  <!--Tabla de Consultoria-->
 
-   <div class="container mt-5">
+  <div class="container mt-5">
     <div class="texto">
       <h2 class="consultoria">Tabla de Consultoria</h2>
     </div>
@@ -18,8 +18,7 @@
         <thead>
           <tr class="encab table-primary">
             <th>No.</th>
-            <th data-bs-toggle="tooltip" data-bs-placement="top"
-              title="Requisitos, iniciativas y buenas práticas de traformación digital(DO/LD)">Requisitos, iniciativas y
+            <th>Requisitos, iniciativas y
               buenas práticas de traformación digital(DO/LD)</th>
             <th>Valor</th>
           </tr>
@@ -27,24 +26,22 @@
         <tbody>
           <tr>
             <td>1</td>
-            <td data-bs-toggle="tooltip" data-bs-placement="top"
-              title="La entidad diagnostica la situación actual y la madurez digital; identifica fortalezas, debilidades, oportunidades, amenazas y riesgos de la transformación digital.">
+            <td>
               La entidad diagnostica la situación actual y la madurez digital; identifica fortalezas, debilidades,
               oportunidades, amenazas y riesgos de la transformación digital.</td>
             <td>
-            <select class="form-select" aria-label="Seleccionar valor">
+              <select class="form-select" aria-label="Seleccionar valor">
                 <option value="0">0</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
                 <option value="4">4</option>
-            </select>
+              </select>
             </td>
           </tr>
           <tr>
             <td>2</td>
-            <td data-bs-toggle="tooltip" data-bs-placement="top"
-              title="La alta dirección asegura que el propósito organizacional es dinámico y sensible al contexto cambiante y digital, con una visión y misión documentadas, y que está disponible para todas las partes interesadas.">
+            <td>
               La alta dirección asegura que el propósito organizacional es dinámico y sensible al contexto cambiante y
               digital, con una visión y misión documentadas, y que está disponible para todas las partes interesadas.
             </td>
@@ -60,8 +57,7 @@
           </tr>
           <tr>
             <td>3</td>
-            <td data-bs-toggle="tooltip" data-bs-placement="top"
-              title="La alta dirección asegura que se identifiquen e involucren las partes interesadas en el contexto digital (p. ej. clientes, personal, proveedores, accionistas e inversionistas, sindicato, reguladores, administración pública, gobierno, etc.) y el ambiente natural.">
+            <td>
               La alta dirección asegura que se identifiquen e involucren las partes interesadas en el contexto digital
               (p. ej. clientes, personal, proveedores, accionistas e inversionistas, sindicato, reguladores,
               administración pública, gobierno, etc.) y el ambiente natural.</td>
@@ -77,8 +73,7 @@
           </tr>
           <tr>
             <td>4</td>
-            <td data-bs-toggle="tooltip" data-bs-placement="top"
-              title="La alta dirección redefine digitalmente el modelo y objetivos de generación de valor, y considera el contexto, expectativas de partes interesadas, marco regulatorio, cambio tecnológico, ambiente natural presente y futuro, asuntos socio-económicos, KPI, etc.">
+            <td>
               La alta dirección redefine digitalmente el modelo y objetivos de generación de valor, y considera el
               contexto, expectativas de partes interesadas, marco regulatorio, cambio tecnológico, ambiente natural
               presente y futuro, asuntos socio-económicos, KPI, etc.</td>
@@ -94,8 +89,7 @@
           </tr>
           <tr>
             <td>5</td>
-            <td data-bs-toggle="tooltip" data-bs-placement="top"
-              title="La alta dirección establece resultados estratégicos digitales claros y compatibles con la dirección estratégica de la organización, considerando contextos interno y externo, políticas de gobernanza digital y las capacidades actuales y futuras de TIC y datos.">
+            <td>
               La alta dirección establece resultados estratégicos digitales claros y compatibles con la dirección
               estratégica de la organización, considerando contextos interno y externo, políticas de gobernanza
               digital y las capacidades actuales y futuras de TIC y datos.</td>
@@ -111,8 +105,7 @@
           </tr>
           <tr>
             <td>6</td>
-            <td data-bs-toggle="tooltip" data-bs-placement="top"
-              title="La entidad define como un objetivo de digitalización la mejora de la experiencia del cliente, por las vías de personalizar y adaptar productos/servicios, brindar atención a medida y omnicanal, así como de los puntos de contacto con partes interesadas. ">
+            <td>
               La entidad define como un objetivo de digitalización la mejora de la experiencia del cliente, por las
               vías de personalizar y adaptar productos/servicios, brindar atención a medida y omnicanal, así como de
               los puntos de contacto con partes interesadas. </td>
@@ -128,8 +121,7 @@
           </tr>
           <tr>
             <td>7</td>
-            <td data-bs-toggle="tooltip" data-bs-placement="top"
-              title="La entidad define como un objetivo de digitalización una mayor flexibilidad y eficiencia en operaciones, que posibiliten la personalización, la creación de productos/servicios digitales y/o la adaptación a las capacidades digitales que demanden los clientes.">
+            <td>
               La entidad define como un objetivo de digitalización una mayor flexibilidad y eficiencia en operaciones,
               que posibiliten la personalización, la creación de productos/servicios digitales y/o la adaptación a las
               capacidades digitales que demanden los clientes.</td>
@@ -155,271 +147,68 @@
 </template>
 
 <style scoped>
-
-/* Sidebar  */
-.sidebar {
-    width: 250px;
-    padding: 15px;
-    transition: transform 0.3s ease;
-    margin-top: 50px;
-    position: fixed;
-    z-index: 100;
-    height: 100%;
-    border-right: 2px solid #020e4a;
-    background-color: rgb(237, 237, 248);
-}
-
-.sidebar ul {
-    list-style-type: none;
-    padding: 1px;
-
-}
-
-.sidebar ul li {
-    margin: 10px 0;
-    background-color: rgb(237, 237, 248);
-    border-color: rgb(0, 0, 102);
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif, 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-}
-
-.sidebar ul li a {
-    color: black;
-    text-decoration: none;
-}
-
-.hidden {
-    transform: translateX(-100%);
-}
-
-.offcanvas {
-    background-color: rgb(237, 237, 248);
-}
-
-/* Header  */
-
-header h1 {
-    color: antiquewhite;
-    font-weight: bold;
-}
-
-header .navbar {
-    background-color: rgb(0, 0, 102);
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif, 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-}
-
-
-header .navbar .container-fluid {
-    background-color: rgb(0, 0, 102);
-    margin: 0;
-}
-
-
-header .navbar .container-fluid .offcanvas-body li a {
-    color: black;
-    font-size: 150%;
-}
-
-header .navbar .container-fluid .offcanvas-body .border {
-    background-color: rgb(0, 0, 102);
-    border-color: rgb(0, 0, 102);
-    width: 90%;
-    height: 3px;
-}
-
-
-
-
-header .navbar .container-fluid #toggle-button {
-    display: none;
-}
-
-
-
-/* Estilos para pantallas pequeñas del Header y el Sidebar*/
-@media (max-width: 990px) {
-    header .navbar .container-fluid #toggle-button {
-        display: block;
-    }
-
-    header .navbar .container-fluid #toggle-button i {
-        color: antiquewhite;
-        background-color: rgb(0, 0, 102);
-    }
-
-    header .navbar .container-fluid button i {
-        color: antiquewhite;
-    }
-
-
-    .sidebar {
-        transform: translateX(-100%);
-        margin-top: 7%;
-        position: absolute;
-        z-index: 100;
-    }
-
-    .sidebar.active {
-        transform: translateX(0);
-        margin-top: 7%;
-    }
-
-}
-
-@media (min-width: 990px) {
-    header .navbar .container-fluid .offcanvas-body li a {
-        color: antiquewhite;
-        font-size: 150%;
-    }
-
-    header .navbar .container-fluid .offcanvas-body .border {
-        display: none;
-    }
-}
-
-
-@media (max-width:587px) {
-    .sidebar.active {
-        margin-top: 11%;
-    }
-
-    .sidebar {
-        margin-top: 11%;
-    }
-
-}
-
 /*Tabla de consultoria*/
 
-.consultoria{
-    text-align: center;
-    font-size: 40px;
+.consultoria {
+  text-align: center;
+  font-size: 40px;
 }
+
 .table {
-    width: 70%;
-    padding: 70px;
-    text-align: center;
-    margin: 150px auto;
-    margin-top: 40px;
-    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-    border: 1px solid rgb(237, 237, 248);
-    background-color: rgb(0, 0, 102);
+  width: 70%;
+  padding: 70px;
+  text-align: center;
+  margin: 150px auto;
+  margin-top: 40px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  border: 1px solid rgb(237, 237, 248);
+  background-color: rgb(0, 0, 102);
 }
 
-.encab{
-    background-color: rgb(0, 0, 102);
+.encab {
+  background-color: rgb(0, 0, 102);
 }
 
-th,td{
-    max-width: 200px;
-    overflow: hidden;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    position: relative;
-}
-form-select{
-    text-align: center;
+th,
+td {
+  max-width: 200px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  position: relative;
 }
 
-.lista_{
-    margin: 70px;
-    padding-left:130px ;
-    margin-top: 100px;
-    font-weight: 700;
-    color: rgb(0, 0, 102);
-    list-style-type: none;
-}
-.row{
-    width: 100%;
+form-select {
+  text-align: center;
 }
 
-/* Footer  */
-footer {
-    padding: 40px 0 20px 0;
-    width: 100%;
-    background-color: rgb(0, 0, 102);
-    display: grid;
-    justify-content: center;
-    align-items: center;
-    justify-self: center;
-    position: absolute;
-    margin-bottom: 0;
-    z-index: 110;
+.lista_ {
+  margin: 70px;
+  padding-left: 130px;
+  margin-top: 100px;
+  font-weight: 700;
+  color: rgb(0, 0, 102);
+  list-style-type: none;
 }
 
-footer .contenedor {
-    display: flex;
-    flex-wrap: wrap;
-    width: 100%;
-    justify-self: center;
-    margin-bottom: 30px;
-    margin-left: 30px;
-    justify-content: center;
-    align-items: center;
-}
-
-footer .button{
-    font-size: 30px;
-    justify-self: center;
-    margin-top: -85px;
-    width: 13%;
-    background-color: #39c;
-    border-radius: 50%;
-    display: flex;
-    justify-content: center;
-}
-
-footer .button i{
-    color: antiquewhite;;
-}
-
-footer .contenedor .redes {
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-
-footer .contenedor .redes img {
-    width: 100%;
-}
-
-footer .contenedor .redes i {
-    color: antiquewhite;
-    font-size: 30px;
-
-}
-
-footer .contenedor .redes{
-    a{
-        margin-left: 7%;
-    }
-     .mail{
-        margin-left: 20%;
-     }
-
-}
-
-footer p{
-
-    color: antiquewhite;
+.row {
+  width: 100%;
 }
 
 /* Estilos para pantallas pequeñas de la Tabla de Consultoria*/
 @media screen and (max-width: 650px) {
-    .table {
-        margin: 60px auto;
-        width: 10%;
-    }
-    .lista_{
-        padding-left: 10px;
-        margin: 40px;
-        margin-top: 60px;
-    }
-@media screen and (max-width: 650px) {
+  .table {
+    margin: 60px auto;
+    width: 10%;
+  }
+
+  .lista_ {
+    padding-left: 10px;
+    margin: 40px;
+    margin-top: 60px;
+  }
+
+  @media screen and (max-width: 650px) {}
 
 }
-
-}
-
-
 </style>
