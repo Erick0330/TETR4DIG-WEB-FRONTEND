e999565a10da40d051dd95dada96636865156f2b
Erick0330 <abdielrl0330@gmail.com>
2024-12-15 22:54:33 -0500

    Coomit

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import PagesContainer from '@/components/PagesContainer.vue'
 import QuiestionsComponent from '@/components/QuiestionsComponent.vue'
 import ReportsComponent from '@/components/ReportsComponent.vue'
-import CompEx from '@/components/CompEx.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,10 +17,6 @@ const router = createRouter({
     {
       path:'/reports',
       component: ReportsComponent
-    },
-    {
-      path:'/try',
-      component:CompEx
     },
 
   ],
